4391a523be15780c2dc639fc14053a833430fd36
avinashDhanani <98724607+avinashDhanani@users.noreply.github.com>
2023-03-05 17:39:38 +0530

    Update Mongoose.js

--- amzone-data/src/DB/Mongoose.js
+++ amzone-data/src/DB/Mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+
 // const mongo =
 //   "mongodb+srv://avinash2808:avinash2808@cluster0.viskioy.mongodb.net/driveDB?retryWrites=true&w=majority";
-const mongo = `mongodb://127.0.0.1:27017/amzone`;
+// const mongo = `mongodb://127.0.0.1:27017/amzone`;
+const mongo = `mongodb+srv://avinashdhanani1:L0Plt1SkKaHzdmjV@cluster0.pekg3sa.mongodb.net/amzoneretryWrites=true&w=majority`;
+
 mongoose
   .connect(mongo, {
     useNewUrlParser: true,
